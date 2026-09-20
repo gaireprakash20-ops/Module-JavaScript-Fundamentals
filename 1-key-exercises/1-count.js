6a65d994cdc22acc104d6068f5a3e724733e137b
gaireprakash20-ops <gaireprakash20@gmail.com>
2026-09-20 22:31:33 +0100

    change the branch

---
+++ 1-key-exercises/1-count.js
@@ -0,0 +1,7 @@
+let count = 0;
+
+count = count + 1;
+
+// Line 1 is a variable declaration, creating the count variable with an initial value of 0
+// Describe what line 3 is doing, in particular focus on what =  reassign a variable using the =  + operator.
+//Line 1 assigns count = 0. Line 3 uses that value (0) and adds 1, so count becomes 1.
