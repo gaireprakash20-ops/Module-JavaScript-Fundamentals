cfc8cf7f90a58825865a41a99eb3353465bfff2b
gaireprakash20-ops <gaireprakash20@gmail.com>
2026-09-26 21:01:25 +0100

    change the name

--- 2-mandatory-errors/0.js
+++ 2-mandatory-errors/0.js
@@ -1,3 +1,3 @@
 /*This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+We don't want the computer to run these 2 lines - how should we solve this problem?
 for single line we used // and for multiple line use */
